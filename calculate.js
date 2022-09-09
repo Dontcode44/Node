50b0b4608b103b82caff706cb4df17e5398b1d4e
Dontcode44 <kabutto378@gmail.com>
2022-09-09 11:45:50 -0500

    Calculate simple with imports

---
+++ calculate.js
@@ -0,0 +1,9 @@
+const math = require('./math.js');
+
+console.log(math);
+/*
+console.log(math.add(8, 2));
+console.log(math.divide(8, 0));
+console.log(math.multiply(8, 0));
+console.log(math.rest(8, 2));
+*/
