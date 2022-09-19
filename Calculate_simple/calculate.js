38b556192be28196ddbca800f630de8500d3e441
Dontcode44 <kabutto378@gmail.com>
2022-09-18 20:01:14 -0500

    Add - Another comment to use 'colors' library 🧶

--- Calculate_simple/calculate.js
+++ Calculate_simple/calculate.js
@@ -1,4 +1,5 @@
 const math = require('./math.js');
+var colors = require('colors');
 
 console.log(math);
 /*
@@ -7,3 +8,5 @@ console.log(math.divide(8, 0));
 console.log(math.multiply(8, 0));
 console.log(math.rest(8, 2));
 */
+/*To use the "colors" library from npm, you should use another parenthesis*/
+console.log(("The result of the rest operation, is: " + math.rest(2, 5)).yellow);
