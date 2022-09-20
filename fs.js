db5f58401150c19ae63d9af472f87b630d2f15d2
Dontcode44 <kabutto378@gmail.com>
2022-09-19 19:41:59 -0500

    Add - Write files with the module, and even send varibales 🐱‍👤

--- fs.js
+++ fs.js
@@ -9,3 +9,13 @@ console.log(first);
 /*This is the file, but with the buffer description, too can use the .toString()
 And you can see the content in String format*/
 console.log(second);
+/*Too use this library to create a file*/
+fs.writeFileSync('./Data/third.txt', 'This is the third file');
+
+const vars = 'This is my text'
+/*Too, u can use the module to send the content of a variable*/
+fs.writeFileSync('./Data/fourth.txt', vars);
+const list = [1, 2, 3, 4, 5]
+/*Even you should send a numbers, but in String format*/
+fs.writeFileSync('./Data/fifth.txt', list.toString());
+
