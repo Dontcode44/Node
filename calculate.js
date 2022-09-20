f64f5b3ea914c3613154449f4039c5134bea98c7
Héctor Andrés <98709351+Dontcode44@users.noreply.github.com>
2022-09-19 20:26:53 -0500

    Delete calculate.js

--- calculate.js
+++
@@ -1,9 +0,0 @@
-const math = require('./math.js');
-
-console.log(math);
-/*
-console.log(math.add(8, 2));
-console.log(math.divide(8, 0));
-console.log(math.multiply(8, 0));
-console.log(math.rest(8, 2));
-*/
